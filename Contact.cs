f3ee99cc66367fb760e78ae67574d830143f81d7
Mihai Iacob <Mihai.Iacob@crimson.co.uk>
2024-04-09 20:28:20 +0100

    Add project files.

---
+++ Contact.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Personal_Details_Processor
+{
+    internal class Contact
+    {
+
+        private string _name;
+        private string _dob;
+        private string _email;
+        private string? _phoneNumber;
+        private DateTime _parsedDOB;
+        private int _age;
+        private string _firstName;
+        private string _lastName;
+
+        //Properties to expose fields for JSON file write
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+        public string Dob => _dob;
+        public int Age => _age;
+        public string Email => _email;
+        public string? PhoneNumber => _phoneNumber;
+
+        //ignore this property for JSON file write
+        [JsonIgnore]
+        public DateTime Birthday => _parsedDOB;
+
+        //constructor for rows which provide all the info we're looking for
+        public Contact(string name, string dOB, string email, string? phoneNumber = null)
+        {
+            _name = name;
+            _dob = dOB;
+            _email = email;
+            _phoneNumber = phoneNumber;
+
+            //parse date of birth and calculate age
+            _parsedDOB = DateTime.Parse(_dob);
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int birthDate = int.Parse(_parsedDOB.ToString("yyyyMMdd"));
+            _age = (now - birthDate) / 10000;
+
+            //get First & Last Name from Name;
+            string[] firstAndLastName = _name.Split(' ');
+            _firstName = firstAndLastName[0];
+            _lastName = firstAndLastName[1];
+
+            //adding white space to the mobile number
+            if (_phoneNumber != null && _phoneNumber.Length > 5)
+            {
+                _phoneNumber = _phoneNumber.Insert(5, " ");
+                //removing excess characters if phone number too long
+                if (_phoneNumber.Length > 12)
+                {
+                    _phoneNumber = _phoneNumber.Substring(0, 12);
+                }
+            }
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("{0, -15} {1, -15} {2, -15} {3, -4} {4, -30} {5, 0}", $"{_firstName}", $"{_lastName}", $"{_dob}", $"{_age}", $"{_email}", $"{_phoneNumber}");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.Json;
+
+namespace Personal_Details_Processor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //read .txt file & remove any blank or null results.
+            //Code will find the text file relative to the project directory
+            string projectFolder = Directory.GetCurrentDirectory();
+            string grandParentDirectory = Directory.GetParent(Directory.GetParent(projectFolder).FullName).FullName;
+            string filePath = Path.Combine(grandParentDirectory, @"docs\contacts.txt");
+            string[] contactDetails = File.ReadAllLines(filePath);
+
+            //cleanup the data.
+            contactDetails = contactDetails.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+            //create a list of contacts
+            List<Contact> contactsList = new List<Contact>();
+
+
+            //add data into each Contact instance
+            for (int i = 0; i < contactDetails.Length; i++)
+            {
+                //add results to new array and split by tab spacing
+                string[] parsedContact = contactDetails[i].Split('\t', StringSplitOptions.RemoveEmptyEntries);
+
+
+                //check if phone number exists
+                bool PhoneNull = parsedContact.ElementAtOrDefault(3) == null;
+
+                //create Contact objects based on bool check above
+                if (PhoneNull)
+                {
+                    contactsList.Add(new Contact(parsedContact[0], parsedContact[1], parsedContact[2]));
+                } else
+                {
+                    contactsList.Add(new Contact(parsedContact[0], parsedContact[1], parsedContact[2], parsedContact[3]));
+                }
+
+            }
+
+            //sort the Contacts by date of birth using LINQ - oldest first
+            var result = (from c in contactsList
+                          orderby c.Birthday
+                          select c);
+
+            //LOOK into String Interpolation to rewrite the 2 lines of code below.
+
+            Console.WriteLine("{0, -15} {1, -15} {2, -15} {3, -4} {4, -30} {5, 0}", $"{"First Name"}", $"{"Last Name"}", $"{"Date of Birth"}", $"{"Age"}", $"{"Email"}", $"{"Phone Number"}");
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+
+            foreach (var p in result)
+            {
+                p.PrintDetails();
+            }
+
+            //print list contents to JSON file - stored in /bin/docs - contacts.json
+
+            //define file path for JSON file
+            string jsonPath = Path.Combine(grandParentDirectory, @"docs\contacts.json");
+            //JSON printing options for indentation
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            //serialize to JSON
+            string json = JsonSerializer.Serialize(contactsList, options);
+            //write to file
+            File.WriteAllText(jsonPath, json);
+
+
+            Console.WriteLine("\nContacts Parsed!\nJSON file generated!\nPress any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
